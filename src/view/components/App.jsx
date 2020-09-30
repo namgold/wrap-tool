b44583bfa7830291b2c622ddad57cb5560c3eb70
Dominik Nam Nguyen <dominik.nam.nguyen@somotsoftvn.com>
2020-09-30 10:26:35 +0700

    initial

--- src/view/components/App.jsx
+++ src/view/components/App.jsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from '../img/logo.svg';
 import '../css/App.css';
 import T from '../js/common';
+const $ = window.$;
 
 class App extends React.Component {
 
-    testConnect(e) {
-        e.preventDefault();
-        T.get("/",
-            data => T.notify("API connect success", T.NOTIFY_TYPE.SUCCESS),
-            error => T.notify("API connect fail", T.NOTIFY_TYPE.DANGER));
+    convert(e) {
+        const input = $('#input').val();
+        if (!input) return;
+        let width = $('#width').val();
+        if (isNaN(width)) return;
+        width = Number(width);
+        const output = input.split('\n').map(i => i.slice(1)).join('').match(new RegExp('.{1,'+width+'}', 'g')).map(i => ' ' + i).join('\n');
+        $('#output').val(output);
+    }
+
+    copy() {
+        var copyText = document.getElementById("output");
+
+        copyText.select();
+        copyText.setSelectionRange(0, 99999999);
+
+        document.execCommand("copy");
+
+        T.notify('Copied', 'Success');
+    }
+
+    selectAll() {
+        var copyText = document.getElementById("output");
+
+        copyText.select();
+        copyText.setSelectionRange(0, 99999999);
     }
 
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Edit <code>src/view/components/App.js</code> and save to reload.</p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-                    <br/>
-                    <button className='btn btn-success' onClick={this.testConnect}>Test API connection</button>
+                    <h1>Wrap text tool</h1>
+                    <div className='row' style={{ height: '500px', width: '1800px' }}>
+                            <div className='ta input col-5'>
+                                <div>
+                                    <label htmlFor='input'>Input</label>
+                                    <label>
+                                    Width:&nbsp;
+                                        <input id='width' type='Number' defaultValue={70} max={200} min={1}/>
+                                    </label>
+                                </div>
+                                <textarea id='input'/>
+                            </div>
+                        <div className='convert-container col-2'>
+                            <button className='btn btn-success convert-button' onClick={this.convert}>Convert</button>
+                        </div>
+                        <div className='ta output col-5'>
+                            <div>
+                                <label htmlFor='output'>Output</label>
+                                <div>
+                                    <button className='btn btn-warning' onClick={this.selectAll}>Select all</button>
+                                    <button className='btn btn-success' onClick={this.copy}>Copy</button>
+                                </div>
+                            </div>
+                            <textarea id='output' readOnly={true}/>
+                        </div>
+                    </div>
                 </header>
             </div>
         );
